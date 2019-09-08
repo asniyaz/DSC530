ec89632e50ac63014b3265be8c86396640eb5bb1
asniyaz <51541928+asniyaz@users.noreply.github.com>
2019-09-08 01:07:10 -0400

    Add files via upload

---
+++ DSC530AhmedNiyazWK2.py
@@ -0,0 +1,33 @@
+# week 2 assignment
+# Ahmed Niyaz
+# DSC - 530
+
+print("Hello World")
+
+x = int(2+2)
+print (x)
+
+y = int(4-2)
+print(y)
+
+z = int(2*2)
+print(z)
+
+a = float(10/2)
+print(a)
+
+b = "data science"
+h = "project 1"
+
+print(str(b)+str(h))
+
+list1 = [1,5,8,9]
+
+print(list1)
+
+list1.append(11)
+
+print(list1)
+
+tuple1 = ("data","science","rocks")
+print(tuple1)
